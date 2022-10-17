5aeec40199b27e9df791c8de4e71d646d24e1505
Ian Simpson <ianbsimpson@gmail.com>
2022-10-17 12:09:59 -0700

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            int a = 17;
+
+            int b = 4;
+            
+            int quotient = a / b ;
+
+            int remainder = a % b; 
+
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
         }
     }
 }

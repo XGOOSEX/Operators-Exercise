7ded1e7255321b90dfb9d382d5b6143815c1c263
Ian Simpson <ianbsimpson@gmail.com>
2022-10-18 09:48:29 -0700

    initial Commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -3,18 +3,30 @@
     internal class Program
     {
         
-        static void Main(string[] args)
+        static void Main()
         {
             
-            int a = 17;
+            //int a = 17;
 
-            int b = 4;
+            //int b = 4;
             
-            int quotient = a / b ;
+            //int quotient = a / b ;
 
-            int remainder = a % b; 
+            //int remainder = a % b;
 
-            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+            //int product = a * b;
+
+            //int sum = a + b;
+
+            //Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
+            
+            Console.WriteLine("What is the radius of the circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            var circle = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of the circle is {circle}");
 
         }
     }
